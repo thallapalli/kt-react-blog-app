aed3feb2539227ea5211ad7a5fc8085ec602ec40
karnakar <thallapallikarnakar@gmail.com>
2023-12-25 17:55:52 -0600

    inital commit with navigation ang route added

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="Header">
+    <h1>Header</h1>
+</header>
+  )
+}
+
+export default Header
---
+++ src/NewPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewPost = () => {
+  return (
+    <main>
+    
+    <h1>New Post</h1>
+    </main>
+  )
+}
+
+export default NewPost
